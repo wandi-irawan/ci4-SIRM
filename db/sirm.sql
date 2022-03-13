7e6f6f9a518a62c2b375c807c1426438503061e8
wandi-irawan <andiimron43@gmail.com>
2022-03-13 20:08:56 +0700

    backup db

--- db/sirm.sql
+++ db/sirm.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2022 at 03:17 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.24
+-- Generation Time: Mar 13, 2022 at 02:04 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `sirm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id_dokter` int(10) UNSIGNED NOT NULL,
+  `nama_dokter` varchar(100) NOT NULL,
+  `spesialis` varchar(100) NOT NULL,
+  `no_telepon` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `tanggal_input` datetime DEFAULT NULL,
+  `tanggal_ubah` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id_dokter`, `nama_dokter`, `spesialis`, `no_telepon`, `email`, `foto`, `tanggal_input`, `tanggal_ubah`) VALUES
+(10, 'wijayaaaaaaaaaa', 'kocheng', '0823453535', 'wandi@gmail.com', '1646405906_d7b0f29c6db3ea25e596.png', '2022-03-04 08:58:26', '2022-03-04 09:26:07');
 
 -- --------------------------------------------------------
 
@@ -42,8 +66,27 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-(3, '2022-02-18-152833', 'App\\Database\\Migrations\\PoliMigration', 'default', 'App', 1645628735, 1),
-(4, '2022-02-23-144838', 'App\\Database\\Migrations\\PasienMigration', 'default', 'App', 1645628735, 1);
+(8, '2022-02-18-152833', 'App\\Database\\Migrations\\PoliMigration', 'default', 'App', 1646318057, 1),
+(9, '2022-02-23-144838', 'App\\Database\\Migrations\\PasienMigration', 'default', 'App', 1646318057, 1),
+(10, '2022-02-28-011120', 'App\\Database\\Migrations\\ObatMigration', 'default', 'App', 1646318058, 1),
+(11, '2022-03-03-142639', 'App\\Database\\Migrations\\DokterMigration', 'default', 'App', 1646318058, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(10) UNSIGNED NOT NULL,
+  `nama_dokter` varchar(100) NOT NULL,
+  `nama_obat` varchar(100) NOT NULL,
+  `dosis` varchar(100) NOT NULL,
+  `aturan_pakai` varchar(100) NOT NULL,
+  `jumlah` varchar(100) NOT NULL,
+  `tanggal_input` datetime DEFAULT NULL,
+  `tanggal_ubah` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -53,11 +96,11 @@ INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`
 
 CREATE TABLE `pasien` (
   `id_pasien` int(10) UNSIGNED NOT NULL,
-  `no_rm` varchar(10) NOT NULL,
-  `nik` varchar(10) NOT NULL,
+  `no_rm` varchar(20) NOT NULL,
+  `nik` varchar(20) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `alamat` text NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
   `agama` varchar(20) NOT NULL,
   `diagnosa` varchar(50) NOT NULL,
   `jenis_rawat` varchar(50) NOT NULL,
@@ -74,7 +117,7 @@ CREATE TABLE `pasien` (
 --
 
 INSERT INTO `pasien` (`id_pasien`, `no_rm`, `nik`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `diagnosa`, `jenis_rawat`, `biaya`, `email`, `no_telepon`, `foto`, `tanggal_input`, `tanggal_ubah`) VALUES
-(1, '001', '123456789', 'Dika Gunawan', 'Kayuara Sekayu', 'Laki-Laki', 'Islam', 'Demam', 'Rawat Jalan', '50000', 'dika@gmail.com', '08234567890', 'dika.jpg', '2022-02-25 14:35:44', '2022-02-25 14:35:44');
+(1, '1234567890', '1234567890123456', 'wandi irawan', 'sekayuagung', 'Laki-Laki', 'Islam', 'kapal selam', 'Rawat Jalan', '200.000', 'wandi@gmail.com', '08888888888', '1646403226_e8e4056b1089a5c2c726.png', '2022-03-04 08:13:46', '2022-03-04 08:30:08');
 
 -- --------------------------------------------------------
 
@@ -90,21 +133,26 @@ CREATE TABLE `poli` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `poli`
---
-
-INSERT INTO `poli` (`id_poli`, `nama_poli`, `tanggal_input`, `tanggal_ubah`) VALUES
-(1, 'Poli 1', '2022-02-23 09:12:03', '2022-02-23 09:12:03');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id_dokter`);
 
 --
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
 
 --
 -- Indexes for table `pasien`
@@ -126,10 +174,22 @@ ALTER TABLE `poli`
 --
 
 --
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id_dokter` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `pasien`
@@ -141,7 +201,7 @@ ALTER TABLE `pasien`
 -- AUTO_INCREMENT for table `poli`
 --
 ALTER TABLE `poli`
-  MODIFY `id_poli` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_poli` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
